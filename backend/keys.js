8b385bfaf883af4ee454a09f507e8f2fd1f758f9
vicky9168 <dholevivek1111@gmail.com>
2023-01-25 23:28:56 +0530

    first commit

---
+++ backend/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoUrl: "mongodb+srv://vivek-admin:vivek12@instagram.nw67q4s.mongodb.net/?retryWrites=true&w=majority",
+    Jwt_secret: "insta"
+
+}
+// ;mongodb+srv://vicky1:vicky12@cluster0.8dndcmz.mongodb.net/?retryWrites=true&w=majority
+// mongodb+srv://user-vicky:vicky@instagram-clone.pbkddy1.mongodb.net/?retryWrites=true&w=majority
